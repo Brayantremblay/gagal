18fea2a53671b19e5730e82eb1e4f4da55d6cb48
Brayam Tremblay <145961105+Brayantremblay@users.noreply.github.com>
2023-12-28 13:22:45 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'cad6b73c-d21d-4c4b-ba48-219306960988';
 
 const proxyIPs = ['linode.id.db-sm.us.to'];
 
